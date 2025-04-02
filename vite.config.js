35f55fcc11cfe1fab3e6b40a945502a65f5186eb
Arnaldo Victor Yamawaki Alves <arnaldoalves34@gmail.com>
2025-04-02 02:39:18 -0300

    alterado a url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/higoramaral/', // Define o caminho base do projeto
+  base: '/higor-amaral/', // Define o caminho base do projeto
   plugins: [vue()]
 });
